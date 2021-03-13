ad055dc17443757fa2206a71454ff437b0058c0f
danielbohnemann <69919600+DanielBohnemann@users.noreply.github.com>
2021-03-13 14:50:18 -0600

    problem2

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,21 @@
+/*
+let continues = true
+cities = []
+
+while (continues == true) {
+  yesNo = prompt("Do you have another city to enter: ")
+  if (yesNo == "yes") {
+  city = prompt("Enter a city: ")
+  cities.push(city)
+} else {
+  continues = false
+  }
+  }
+
+count = 0
+while(cities.length !== count) {
+  let name = cities[count]
+  console.log(name.toLowerCase())
+  count ++
+  }
+*/
---
+++ forms/dogStore/dogStore.js
@@ -0,0 +1,41 @@
+/*
+let products = [
+  "Crocheted Dog Toy",
+  "Slow Feeder Bowl",
+  "Pawz Grey Treat Jar",
+  "Blue Sweet Dreams Thermal Pajamas",
+  "Checker Chewy Vuiton Bowl",
+  "Red Heart Sweater Dress",
+  "Velvet Hair Bow - Sun Kissed",
+  "Into the Wild Leash"
+]
+
+let prices = [
+  10,
+  14,
+  20,
+  30,
+  32,
+  27,
+  12,
+  27
+] 
+
+
+let wishList = []
+let item = []
+
+showBtn.onclick=function(){
+  for (let i = 0; i <= prices.length -1; i++) {
+    item.push(`${products[i]} : ${prices[i]}`)
+    }
+    lbl.value= `${item}`
+    }
+
+btn.onclick=function(){
+  let product = items.value
+  wishList.push(product)
+  wishes.value  = `${wishList}`
+}
+*/
+
